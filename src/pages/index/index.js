cdb97f710f78f628aa461f40a300900ee67b001e
islandsaying <47809373+islandsaying@users.noreply.github.com>
2019-07-04 10:15:24 +0800

    首页和底部
    first commit
    数据渲染获取和部署,触底加载
    首页和底部组件

---
+++ src/pages/index/index.js
@@ -0,0 +1,54 @@
+import 'css/common.css'
+import './index.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import MintUI from 'mint-ui'
+
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll);
+
+import Foot from 'components/Foot'
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        lists: null,
+        pageNum: 1,
+        pageSize: 6,
+        loading: false, //true表示可以加载
+        allLoaded: false //是否完全加载
+    },
+    created(){
+        this.getLists()
+    },
+    methods: {
+        //是否在加载中
+        getLists() {
+            if(this.allLoaded) return;
+            this.loading = true; //不作重复请求
+            axios.get(url.hotLists,{
+                pageNum : this.pageNum,
+                pageSize: this.pageSize
+            }).then(res => {
+                let curLists = res.data.lists
+                //判断所有数据是否加载完毕
+                if(curLists.length < this.pageSize){
+                    this.allLoaded = true
+                }
+                if(this.lists) {
+                    this.lists = this.lists.concat(curLists)
+                } else{
+                    //第一次请求数据
+                    this.lists = curLists
+                }
+                this.loading = false  //请求完之后才允许再次请求
+                this.pageNum++
+            })
+        }
+    },
+    components:{
+        Foot
+    }
+})
