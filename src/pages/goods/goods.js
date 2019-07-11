8553fff0044aa31c78b61c1fa893c38672fc0911
islandsaying <47809373+islandsaying@users.noreply.github.com>
2019-07-11 21:12:40 +0800

    add transition for goods

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -4,6 +4,7 @@ import './goods.css'
 import './goods_theme.css'
 import './goods_mars.css'
 import './goods_sku.css'
+import './goods_transition.css'
 
 import Vue from 'vue'
 import url from 'js/api.js'
@@ -20,11 +21,17 @@ let detailTab = ['商品详情','本店成交']
 new Vue({
     el:'#app',
     data:{
+        id,
         details: null,
         detailTab,
         tabIndex: 0, 
         dealLists: null,
-        bannerLists: null
+        bannerLists: null,
+        skuType: 1,
+        showSku: false,
+        skuNum: 1,
+        isAddCart : false , //有没有加入购物车
+        showAddMessage: false
     },
     created(){
         this.getDetails()
@@ -56,6 +63,30 @@ new Vue({
                 this.getDeal()
             }
         },
+        chooseSku(type){
+            this.skuType = type
+            this.showSku = true
+        },
+        changeSkuNum(num){
+            if(num<0 && this.skuNum === 1)return
+            this.skuNum === 1
+        },
+        addCart(){
+            axios.post(url.addCart,{
+                id,
+                number: this.skuNum
+            }).then(res => {
+                if(res.data.status === 200){
+                    this.showSku = false
+                    this.isAddCart = true
+                    this.showAddMessage = true
+
+                    setTimeout(() =>{
+                        this.showAddMessage = false
+                    },800)
+                }
+            })
+        }
     },
     filters:{
         numFilter(price){
@@ -64,5 +95,13 @@ new Vue({
     },
     components:{
         Swipe
+    },
+    watch:{
+        showSku(val,oldVal){
+            document.body.style.overflow = val ? 'hidden': 'auto'
+            document.querySelector('html').style.overflow = val ? 'hidden' : 'auto'
+            document.body.style.height = val ? '100%' : 'auto'
+            document.querySelector('html').style.height = val ? '100%' : 'auto'
+        }
     }
 })
